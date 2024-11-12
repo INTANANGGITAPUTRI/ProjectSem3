49b27a269c1816c24d6e25fd23323b0c04fead74
afriz-firza <firzaafrizalazka@gmail.com>
2024-11-12 10:06:27 +0700

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -2,7 +2,7 @@
 $servername = "127.0.0.1";  // Change to your server name if different
 $username = "root";         // Change to your database username
 $password = "";             // Change to your database password
-$dbname = "website_2_admin_user_2";
+$dbname = "db_connect";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
